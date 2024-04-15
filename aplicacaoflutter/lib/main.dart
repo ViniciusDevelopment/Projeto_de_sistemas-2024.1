d4655c8d0b5aa681be51c3d23dde7b4c5d2043db
NOTEBOOK-VINICI\VINICIUS DEVELOPMENT <viniciusmacielpires@gmail.com>
2024-04-15 12:48:36 -0300

    feature cadastro servico

--- aplicacaoflutter/lib/main.dart
+++ aplicacaoflutter/lib/main.dart
@@ -4,6 +4,7 @@ import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'index.dart';
 
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
