0d54a142474f54b14cd8e7995f9e10d784a5faa3
beneX90 <benedito1502@gmail.com>
2024-06-02 17:30:28 -0300

    Esqueceu a senha funcional

---
+++ aplicacaoflutter/lib/PagesCommon/ForgotPassword.dart
@@ -0,0 +1,102 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  final TextEditingController _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future passwordReset() async {
+    try {
+      await FirebaseAuth.instance
+        .sendPasswordResetEmail(email: _emailController.text.trim());
+      showDialog(
+        context: context, 
+        builder: (context){
+          return AlertDialog(
+            content: Text('Link de redefinição de senha enviado! Cheque o seu email'),
+          );
+        }
+        );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        inputFile(label: "Digite o seu email para alterar a senha", controller: _emailController),
+        Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Container(
+                      padding: EdgeInsets.only(top: 3, left: 3),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+
+                        onPressed: passwordReset,
+                        
+                        color: Color(0xff0095FF),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Text(
+                          "Enviar email",
+                          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18, color: Colors.white),
+                        )
+                      ),
+      ),
+    ),],));
+  }
+}
+
+Widget inputFile({required String label, bool obscureText = false, required TextEditingController controller}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey[400]!),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey[400]!),
+          ),
+        ),
+      ),
+      SizedBox(height: 10),
+    ],
+  );
+}
