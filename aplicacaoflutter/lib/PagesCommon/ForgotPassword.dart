d3f25c03b173e770a2c57c3afae8695644c45389
beneX90 <benedito1502@gmail.com>
2024-06-02 23:26:56 -0300

    pendencias atualizadas

--- aplicacaoflutter/lib/PagesCommon/ForgotPassword.dart
+++ aplicacaoflutter/lib/PagesCommon/ForgotPassword.dart
@@ -20,18 +20,17 @@ class _ForgotPasswordState extends State<ForgotPassword> {
   Future passwordReset() async {
     try {
       await FirebaseAuth.instance
-        .sendPasswordResetEmail(email: _emailController.text.trim());
+          .sendPasswordResetEmail(email: _emailController.text.trim());
       showDialog(
-        context: context, 
-        builder: (context){
-          return AlertDialog(
-            content: Text('Link de redefinição de senha enviado! Cheque o seu email'),
-          );
-        }
-        );
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text(
+                  'Link de redefinição de senha enviado! Cheque o seu email'),
+            );
+          });
     } on FirebaseAuthException catch (e) {
       print(e);
-      
     }
   }
 
@@ -48,54 +47,64 @@ class _ForgotPasswordState extends State<ForgotPassword> {
           icon: Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
         ),
       ),
-      body: Padding( // Adicionando Padding aqui
-  padding: EdgeInsets.symmetric(horizontal: 20.0), // Define a margem horizontal
-  child: Container(
-    height: MediaQuery.of(context).size.height,
-    width: double.infinity,
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      inputFile(label: "Digite o seu email para alterar a senha", controller: _emailController),
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 40),
+      body: Padding(
+        // Adicionando Padding aqui
+        padding: EdgeInsets.symmetric(
+            horizontal: 20.0), // Define a margem horizontal
         child: Container(
-          padding: EdgeInsets.only(top: 3, left: 3),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            border: Border.all(color: Colors.black),
-          ),
-          child: MaterialButton(
-            minWidth: double.infinity,
-            height: 60,
-            onPressed: passwordReset,
-            color: Color(0xff0095FF),
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50),
-            ),
-            child: Text(
-              "Enviar email",
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18, color: Colors.white),
-            )
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              inputFile(
+                  label: "Digite o seu email para redefinir a sua senha",
+                  controller: _emailController),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Container(
+                  padding: EdgeInsets.only(top: 3, left: 3),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: passwordReset,
+                      color: Color(0xff0095FF),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Text(
+                        "Enviar email",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.white),
+                      )),
+                ),
+              ),
+            ],
           ),
         ),
       ),
-    ],
-    ),
-  ),
-),
     );
   }
 }
 
-Widget inputFile({required String label, bool obscureText = false, required TextEditingController controller}) {
+Widget inputFile(
+    {required String label,
+    bool obscureText = false,
+    required TextEditingController controller}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: <Widget>[
       Text(
         label,
-        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+        style: TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
       ),
       SizedBox(
         height: 5,
