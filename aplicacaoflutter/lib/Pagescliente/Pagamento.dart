b575666c262f82420cf09c8bb72477b31d534d4a
Daniel Noleto <daniel.noleto@mail.uft.edu.br>
2024-06-29 18:48:29 -0300

    ajuste taxa de serviço

--- aplicacaoflutter/lib/Pagescliente/Pagamento.dart
+++ aplicacaoflutter/lib/Pagescliente/Pagamento.dart
@@ -9,62 +9,59 @@ class PagamentoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double taxa = int.parse(service['valorcliente']) - 5;
+    double valorCliente = double.parse(service['valorcliente']);
+    double taxa = valorCliente * 0.05;
+    String taxaFormatada = taxa.toStringAsFixed(2);
+    valorCliente = valorCliente - taxa;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Efetuar Pagamento'),
         backgroundColor: Colors.white,
       ),
-
       body: Column(
         children: [
-        Container(
-          padding: const EdgeInsets.only(top: 24),
-          alignment: Alignment.center,
-          child: const Text("Valor a ser pago:"),
-        ),
-
-        Container(
-          alignment: Alignment.center,
-          child: Text("R\$${service['valorcliente']},00", style: const TextStyle(fontSize: 50),),
-        ),
-
-         Container(
-          padding: const EdgeInsets.only(top: 12),
-          alignment: Alignment.center,
-          child: Text("R\$$taxa,00 do serviço + R\$5,00 de taxa da aplicação", style: const TextStyle(fontSize: 10),),
-        ),
-
-
-        Column(
-          children: [
-        
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-              child: const Text("Métodos de Pagamento:", style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue
-              ),)
+          Container(
+            padding: const EdgeInsets.only(top: 24),
+            alignment: Alignment.center,
+            child: const Text("Valor a ser pago:"),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Text("R\$${service['valorcliente']},00", style: const TextStyle(fontSize: 50),),
+          ),
+          Container(
+            padding: const EdgeInsets.only(top: 12),
+            alignment: Alignment.center,
+            child: Text("R\$$valorCliente do serviço + R\$$taxaFormatada de taxa da aplicação", style: const TextStyle(fontSize: 10),),
+          ),
+          Column(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                child: const Text("Métodos de Pagamento:", style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue
+                ),),
               ),
-        
-               ListTile(
+              ListTile(
                 leading: const Icon(Icons.credit_card),
                 title: const Text("Cartão de Débito"),
                 onTap: () {
                   Navigator.push(context,  MaterialPageRoute(builder: (context) => Debito(service)),);
                 },
               ),
-               ListTile(
+              ListTile(
                 leading: const Icon(Icons.pix),
                 title: const Text("Pix"),
                 onTap: () {
                   Navigator.push(context,  MaterialPageRoute(builder: (context) => Pix(service)),);
                 },
               )
-          ],
-        ),
+            ],
+          ),
         ],
       ),
     );
